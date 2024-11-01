16ee5f19fc3954fb80004ba5e376587b4c30aa0d
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-11-01 22:58:38 +0200

    modified to use nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import {Link} from 'expo-router';
+import "../global.css";
+
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Welcome to Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style ={{color:'blue'}}>Go to Profile
+      <Link href="/profile" style ={{color:'blue'}}>
+      Go to Profile
       </Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
